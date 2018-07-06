a742d923d6498a81525f3065c2d589d04d1ba305
Gend Smith <1439786496@qq.com>
2018-07-06 14:09:23 +0800

    添加 防抖函数

---
+++ 防抖函数/debounce.js
@@ -0,0 +1,25 @@
+//防抖函数
+var debounce = function(callback,gapTime) {
+    var timer = null;
+    return function() {
+        clearTimeout(timer);
+        timer = setTimeout(callback,gapTime);
+    }
+}
+//注意这里监听document的scroll操作来触发函数
+var nothing = function () {
+    document.getElementById('nothing').onscroll = function () {
+        console.log("普通滚动")
+    }
+}
+
+var debounceScroll = function () {
+    document.getElementById('debounce').onscroll = 
+        debounce(() => {
+            console.log('节流滚动')
+        },300)
+}
+
+nothing();
+
+debounceScroll();
